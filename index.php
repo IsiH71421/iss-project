e352674a4fb208f74db79bb8d58c3ce8941cdc90
Isabell Helling <isabell.helling@gmail.com>
2025-06-22 13:32:50 +0200

    show current ISS position

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
-  header("Location: display/index.html");
-  exit;
+  $requestUri = $_SERVER['REQUEST_URI'];
+
+// Weiterleitung nur wenn exakt /iss/ oder /iss (ohne Dateinamen) aufgerufen wird
+  if ($requestUri === '/~ge25fel/iss/' || $requestUri === '/~ge25fel/iss') {
+    header("Location: display/index.html");
+    exit;
+  }
+// sonst keine Weiterleitung, andere Seiten können geladen werden
 ?>
