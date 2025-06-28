314794dbef9522065f534d85a5e10eff75c43b5e
Isabell Helling <isabell.helling@gmail.com>
2025-06-28 14:18:32 +0200

    add qr codes and zoom parameter to position html

---
+++ control/zoom-out.php
@@ -0,0 +1,13 @@
+<?php
+$cb = file_get_contents('callback.url');
+$data = array("zoom" => "zoom-out");
+$opts = array(
+  'http'=>array(
+    'method'=>"PUT",
+    'header'=>"content-type: application/json\r\n",
+    'content' => json_encode($data)
+  )
+);
+$context = stream_context_create($opts);
+$resp = file_get_contents($cb, false, $context);
+?>
